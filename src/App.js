4ade53ee11e459213397a27b408e0291e9adb22f
Dan Rossano <dannyboy.rossano@gmail.com>
2022-10-24 22:08:52 -0700

    notes backend/frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+BrowserRouter as Router,
+Routes, 
+Route } from 'react-router-dom'
+
+import Notes from './pages/Notes'
+import Note from './pages/Note'
+import Layout from './components/Layout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+      <Route path='/' element={<Notes/>} exact />
+      <Route path='/:id' element={<Note/>} />
+      </Routes>
+      </Layout>
+    </Router>
   );
 }
 
